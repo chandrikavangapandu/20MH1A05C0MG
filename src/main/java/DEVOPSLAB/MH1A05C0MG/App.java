cd6fd578dc7d52f0c7ba7e24b660072b9526493a
chand <chand@MSI>
2022-11-17 22:13:35 +0530

    hii

---
+++ src/main/java/DEVOPSLAB/MH1A05C0MG/App.java
@@ -0,0 +1,13 @@
+package DEVOPSLAB.MH1A05C0MG;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/DEVOPSLAB/MH1A05C0MG/AppTest.java
@@ -0,0 +1,20 @@
+package DEVOPSLAB.MH1A05C0MG;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
